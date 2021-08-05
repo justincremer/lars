16c442e3060324496f053c63cf94d024b5e8d2da
justincremer <jacremer@live.com>
2021-08-05 16:19:22 -0700

    base linear algebra functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,72 @@
-use std::ops::{Add, Mul, Sub};
+use std::ops::{Add, Div, Mul, Sub};
 
-// Defines linear interpolability of a given data primitive in a higher dimension
+// Linearly interpolate a set of primitives in a higher dimension
 pub fn lerp<T>(n: [T; 3]) -> T
 where
-    T: Copy + Sub<Output = T> + Mul<Output = T> + Add<Output = T>,
+    T: Copy + Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Div<Output = T>,
 {
     n[0] + (n[1] - n[0]) * n[2]
 }
+
+pub trait Interporable<T>
+where
+    T: Copy + Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Div<Output = T>,
+{
+    fn new(xs: &[T]) -> Self;
+    fn from_single(n: T, s: usize) -> Self;
+    fn sum(&self, other: Vec<T>) -> Self;
+    fn sub(&self, other: Vec<T>) -> Self;
+    fn mul(&self, other: Vec<T>) -> Self;
+    fn div(&self, other: Vec<T>) -> Self;
+}
+
+impl<T> Interporable<T> for Vec<T>
+where
+    T: Copy + Add<Output = T> + Sub<Output = T> + Mul<Output = T> + Div<Output = T>,
+{
+    fn new(xs: &[T]) -> Self {
+        Vec::from(xs)
+    }
+
+    fn from_single(n: T, s: usize) -> Self {
+        vec![n; s]
+    }
+
+    fn sum(&self, other: Vec<T>) -> Self {
+        self.iter()
+            .enumerate()
+            .map(|(i, n)| *n + other[i])
+            .collect()
+    }
+
+    fn sub(&self, other: Vec<T>) -> Self {
+        self.iter()
+            .enumerate()
+            .map(|(i, n)| *n - other[i])
+            .collect()
+    }
+
+    fn mul(&self, other: Vec<T>) -> Self {
+        self.iter()
+            .enumerate()
+            .map(|(i, n)| *n * other[i])
+            .collect()
+    }
+
+    fn div(&self, other: Vec<T>) -> Self {
+        self.iter()
+            .enumerate()
+            .map(|(i, n)| *n / other[i])
+            .collect()
+    }
+}
+
+/* TODO:
+v2f_sqrt(V2f v0);
+v2f_pow(V2f v0, V2f v1);
+v2f_sin(V2f v0);
+v2f_cos(V2f v0);
+v2f_min(V2f v0, V2f v1);
+v2f_max(V2f v0, V2f v1);
+v2f_lerp(V2f v0, V2f v1, V2f v2);
+*/
